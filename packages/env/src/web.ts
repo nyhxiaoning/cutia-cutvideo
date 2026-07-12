34d8e0d88a30fe94979a5874a5430d3e5ae56b56
henry <1540097293@qq.com>
2026-07-12 12:20:32 +0800

    feat: 配置当前的使用

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -19,8 +19,9 @@ const webEnvSchema = z.object({
 	BETTER_AUTH_SECRET: z.string().optional(),
 	UPSTASH_REDIS_REST_URL: z.url(),
 	UPSTASH_REDIS_REST_TOKEN: z.string(),
-	FREESOUND_CLIENT_ID: z.string().optional(),
-	FREESOUND_API_KEY: z.string().optional(),
+	// 默认写入两个配置地址：
+	FREESOUND_CLIENT_ID: z.string().default("xMgo2rK4L7PbQMrSLEwg"),
+	FREESOUND_API_KEY: z.string().default("4I2LAJznhq0A2VKgi3UvMCeMm3SLWbUI82ZNjPHk"),
 
 	// Cloudflare R2
 	R2_ACCOUNT_ID: z.string().optional(),
